ca67eb7df59d199c8188fe5a80d60b96bbcf9fee
EliaSolivari <78438526+EliaSolivari@users.noreply.github.com>
2022-05-20 23:45:18 +0200

    Add files via upload

---
+++ client/Scudo/src/api.js
@@ -0,0 +1,78 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
+import {getFirestore,collection, getDoc, doc} from"https://www.gstatic.com/firebasejs/9.6.11/firebase-firestore.js";
+import credentials from "./credentials.js";
+
+
+
+const app=initializeApp(credentials);
+const db = getFirestore(app);  
+
+const sensors =collection(db,"sensors");
+const sensorsData = doc(sensors,"0");
+const storico=collection(db,"storico");
+
+
+
+var iterations=2;
+var storedValue=-1
+var updating=0
+
+const updateChecker=(doc)=>{
+    
+    if(doc.data().updating!==storedValue && storedValue!==-1){
+        updating=1;
+        iterations=0;
+    }
+
+    else if(iterations<2){
+        iterations++;
+        updating=1;
+    }
+    else if(iterations<5){
+        iterations++;
+        updating=0;
+    }
+    else{
+        updating=-1;
+        iterations=5;
+    }
+    storedValue=doc.data().updating;
+}
+
+let current
+
+function getDb(){
+    return getDoc(sensorsData).then(function(doc){
+        current={hum: doc.data().hum,
+        light:  doc.data().light,
+        temp:  doc.data().temp}
+    });
+}
+
+function getStorico(){
+    let temp=[];
+    for(let i=0;i<10;i++){
+        let docData=doc(storico,i.toString());
+        getDoc(docData).then(function(doc){
+            temp.push({temp:doc.data().temp,hum:doc.data().hum,light:doc.data().light,time:doc.data().time});
+        }
+    );
+    }
+    return temp;
+}
+
+
+
+
+export { current }
+export { getStorico }
+export { updating }
+
+setInterval(()=>{
+    getDoc(sensorsData).then(function(doc){
+        updateChecker(doc);
+        
+    });
+    getDb();
+}
+,1000);
---
+++ client/Scudo/src/credentials.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDXywlaogGD8gRjL-zavGy7gmr8q3ySi1A",
+    authDomain: "scudo-project.firebaseapp.com",
+    projectId: "scudo-project",
+    storageBucket: "scudo-project.appspot.com",
+    messagingSenderId: "950275414922",
+    appId: "1:950275414922:web:962162fda0d50175114673"
+  };
